71c586d4f0030bca4b0b8229e677029edec19cea
JoanGoAl <joangonzalezalbert@gmail.com>
2022-05-22 20:51:13 +0200

    Module Shop (List cars, filters)

--- frontend/module/shop/controller/controller_shop.js
+++ frontend/module/shop/controller/controller_shop.js
@@ -1,10 +1,57 @@
 app.controller('controller_shop', function($scope, services_shop, cars, filters) {
 
-    // $scope.cars = cars
     $scope.brands = filters[0]
+    $scope.colors = filters[2]
 
-    $scope.models = function() {
-        // services_shop.getModels()
-        console.log(this);
+    let filtros = services_shop.setFilters()
+
+    function loadCars() {
+        services_shop.getCars(filtros).then((data => {
+            $scope.cars = data[0];
+        }))
     }
+
+    function highlights() {
+        $scope.brandSelected = filtros.id_brands
+        $scope.modelSelected = filtros.id_models
+    }
+
+    loadCars()
+    highlights()
+
+    $scope.getModels = function() {
+        $scope.modelos = services_shop.getModels(this.brandSelected, filters[1])
+
+        filtros.id_brands = this.brandSelected
+        filtros.id_models = 'Allmodels'
+
+        localStorage.setItem('filtros',  JSON.stringify(filtros))
+
+        loadCars()
+    }
+
+    $scope.loadCarsForModel = function() {
+        filtros.id_models = this.modelSelected
+        localStorage.setItem('filtros',  JSON.stringify(filtros))
+        loadCars()
+    }
+ 
+    $scope.removeFilters = function() {
+        filtros = {
+            id_brands: 'Allbrand',
+            id_models: 'Allmodels',
+            color: 'Allcolors',
+            category: 'Allcategories',
+            city: 'Allcities',
+            bodywork: 'Allbody'
+        }
+        localStorage.setItem('filtros',  JSON.stringify(filtros))
+
+        loadCars()
+        highlights()
+    }
+    
+    // $scope.seeLS = function() {
+    //     $scope.ls = JSON.parse(localStorage.getItem('filtros'))
+    // }
 })
--- frontend/module/shop/services/services_shop.js
+++ frontend/module/shop/services/services_shop.js
@@ -1,12 +1,57 @@
 app.factory('services_shop', ['services', '$rootScope', 'toastr', function(services, $rootScope, toastr) { 
 
     let service = {
-        getModels: getModels
+        getModels: getModels,
+        setFilters: setFilters,
+        getCars: getCars
     }
     return service;
 
-    function getModels() {
-        return 'HOlaa';
+    function getModels(brand, models) {
+        let arry = []
+
+        models.forEach(item => {
+            if (brand == item.id_brand) arry.push(item)
+        })
+
+        return arry;
     }
 
+    function setFilters() {
+        let filtros = {}
+        if (localStorage.getItem('filtros')) {
+            filtros = JSON.parse(localStorage.getItem('filtros'))
+        } else {
+            filtros = {
+                id_brands: 'Allbrand',
+                id_models: 'Allmodels',
+                color: 'Allcolors',
+                category: 'Allcategories',
+                city: 'Allcities',
+                bodywork: 'Allbody'
+            }
+
+            localStorage.setItem('filtros', JSON.stringify(filtros))
+        }
+
+        return filtros
+    }
+
+    function getCars(filtros) {
+        
+        let totalData = {
+            data: filtros,
+            pos: 'def',
+            pagination: 0
+        }
+
+        return services.post('shop', 'filters', totalData)
+            .then(function(response) {
+
+                return response;
+                
+            }, function(error) {
+                console.log(error);
+            });
+    }
 }])
